5f637a47f68df13add254e20622b545e1a0555a0
Cody T <crthorn11@gmail.com>
2024-02-04 09:36:51 -0500

    adding the picture and links to the ReadME

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # myworkdayscheduler
-Has starting code, I need to add jquery to by dynamic
+Has starting code, I need to add jquery to be dynamic. 
+
+# Link to github
+https://github.com/crthorn11/myworkdayscheduler
+
+# Link to deployed application
+https://crthorn11.github.io/myworkdayscheduler/
+
+
+
+![alt text](./assets/img/2024-02-04.png)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,36 +1,37 @@
 $(function () {
-  $('.saveBtn').on('click', function () {
-    localStorage.setItem($(this).parent().attr('id'), $(this).siblings('textarea').val());
-  })
-  $("#hour-9 textarea").val(localStorage.getItem('hour-9'))
-  $("#hour-10 textarea").val(localStorage.getItem('hour-10'))
-  $("#hour-11 textarea").val(localStorage.getItem('hour-11'))
-  $("#hour-12 textarea").val(localStorage.getItem('hour-12'))
-  $("#hour-1 textarea").val(localStorage.getItem('hour-1'))
-  $("#hour-2 textarea").val(localStorage.getItem('hour-2'))
-  $("#hour-3 textarea").val(localStorage.getItem('hour-3'))
-  $("#hour-4 textarea").val(localStorage.getItem('hour-4'))
-  $("#hour-5 textarea").val(localStorage.getItem('hour-5'))
-  $("#hour-6 textarea").val(localStorage.getItem('hour-6'))
-  $("#currentDay").text(dayjs().format('MM/DD/YYYY'));
-});
+  $('.saveBtn').on('click', function(){
+    localStorage.setItem($(this).parent().attr('id'), $(this).siblings('textarea').val() );
+    })
+      $("#hour-9 textarea").val(localStorage.getItem('hour-9'))
+      $("#hour-10 textarea").val(localStorage.getItem('hour-10'))
+      $("#hour-11 textarea").val(localStorage.getItem('hour-11'))
+      $("#hour-1 textarea").val(localStorage.getItem('hour-12'))
+      $("#hour-1 textarea").val(localStorage.getItem('hour-1'))
+      $("#hour-2 textarea").val(localStorage.getItem('hour-2'))
+      $("#hour-3 textarea").val(localStorage.getItem('hour-3'))
+      $("#hour-4 textarea").val(localStorage.getItem('hour-4'))
+      $("#hour-5 textarea").val(localStorage.getItem('hour-5'))
+      $("#hour-6 textarea").val(localStorage.getItem('hour-6'))
+    $("#currentDay").text(dayjs().format('MM/DD/YYYY'));
+  });
 
-const currentHour = dayjs().format('H');
-console.log("currentHour", currentHour);
+  //const calendarHour = dayjs().format('MM/DD/YYY');
+  const calendarHour = dayjs().format('h');
+  const currentHour = dayjs().format('h');
+  
+  const timeBlocks = document.querySelectorAll('.time-block');
 
-const timeBlocks = document.querySelectorAll('.time-block');
+  timeBlocks.forEach((timeBlock) => {
 
-timeBlocks.forEach((timeBlock) => {
+    const calendarHour = timeBlock.getAttribute('id').split('-')[1];
+    console.log("currentHour", currentHour);
+    console.log("calendarHour", calendarHour);
 
-  const calendarHour = timeBlock.getAttribute('id').split('-')[1];
-  console.log("currentHour", currentHour);
-  console.log("calendarHour", calendarHour);
-
-  if (currentHour < calendarHour) {
-    timeBlock.classList.add('past');
-  } else if (calendarHour === currentHour) {
-    timeBlock.classList.add('present');
-  } else {
-    timeBlock.classList.add('future');
-  }
-});
+ if (currentHour < calendarHour) {
+   timeBlock.classList.add('past');
+ } else if (currentHour === calendarHour) {
+   timeBlock.classList.add('present');
+ } else {
+   timeBlock.classList.add('future');
+ }
+ });
