8dc1425589ef4ede58fae3a1bcb76f8dba870c43
Cody T <crthorn11@gmail.com>
2024-02-04 20:00:13 -0500

    Fixed more bugs, final commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,32 +6,38 @@ $(function () {
       $("#hour-10 textarea").val(localStorage.getItem('hour-10'))
       $("#hour-11 textarea").val(localStorage.getItem('hour-11'))
       $("#hour-1 textarea").val(localStorage.getItem('hour-12'))
-      $("#hour-1 textarea").val(localStorage.getItem('hour-1'))
-      $("#hour-2 textarea").val(localStorage.getItem('hour-2'))
-      $("#hour-3 textarea").val(localStorage.getItem('hour-3'))
-      $("#hour-4 textarea").val(localStorage.getItem('hour-4'))
-      $("#hour-5 textarea").val(localStorage.getItem('hour-5'))
-      $("#hour-6 textarea").val(localStorage.getItem('hour-6'))
+      $("#hour-1 textarea").val(localStorage.getItem('hour-13'))
+      $("#hour-2 textarea").val(localStorage.getItem('hour-14'))
+      $("#hour-3 textarea").val(localStorage.getItem('hour-15'))
+      $("#hour-4 textarea").val(localStorage.getItem('hour-16'))
+      $("#hour-5 textarea").val(localStorage.getItem('hour-17'))
+      $("#hour-6 textarea").val(localStorage.getItem('hour-18'))
     $("#currentDay").text(dayjs().format('MM/DD/YYYY'));
   });
 
   //const calendarHour = dayjs().format('MM/DD/YYY');
-  const calendarHour = dayjs().format('h');
-  const currentHour = dayjs().format('h');
+  const calendarHour = dayjs().format('H');
+  const currentHour = parseInt( dayjs().format('H'));
   
   const timeBlocks = document.querySelectorAll('.time-block');
 
   timeBlocks.forEach((timeBlock) => {
 
-    const calendarHour = timeBlock.getAttribute('id').split('-')[1];
+    const calendarHour = parseInt( timeBlock.getAttribute('id').split('-')[1]);
     console.log("currentHour", currentHour);
     console.log("calendarHour", calendarHour);
 
- if (currentHour < calendarHour) {
+ if (currentHour > calendarHour) {
    timeBlock.classList.add('past');
+   timeBlock.classList.remove('present');
+   timeBlock.classList.remove('future');
  } else if (currentHour === calendarHour) {
+   timeBlock.classList.remove('past');
+   timeBlock.classList.remove('future');
    timeBlock.classList.add('present');
  } else {
+   timeBlock.classList.remove('past');
+   timeBlock.classList.remove('present');
    timeBlock.classList.add('future');
  }
  });
